8fcedc94f1a77a51ed48b62d13a546e7715645a2
sunpao2020 <sunpao2040@gmail.com>
2025-07-20 20:38:05 +0700

    Add files via upload

---
+++ 4/3.py
@@ -0,0 +1,7 @@
+print("Log In with your Admin Account")
+u=str(input("กรอก Username: "))
+p=str(input("กรอก Password: "))
+if u == "admin" and p == "Ad13n@23t":
+    print("Welcome, admin")
+else:
+    print("You're not admin")
