16bd8e4f96836d1853389e91630d1b8f34014052
sunpao2020 <sunpao2040@gmail.com>
2025-07-20 20:52:43 +0700

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,38 @@
+count = 0
+i = 0
+for i in range(1, 101):
+    print(i)
+    count += 1
+print("ลูป 1 ทำงาน",(count),"รอบและ i มีค่าเท่ากับ",(i))
+count = 0
+i = 0
+for i in range(7, 77, 7):
+    print(i)
+    count += 1
+print("ลูป 2 ทำงาน",(count),"รอบและ i มีค่าเท่ากับ",(i))
+count = 0
+i = 0
+for i in range(20, 1,-2):
+    print(i)
+    count += 1
+print("ลูป 3 ทำงาน",(count),"รอบและ i มีค่าเท่ากับ",(i))
+count = 0
+i = 0
+for i in range(2, 18, 3):
+    print(i)
+    count += 1
+print("ลูป 4 ทำงาน",(count),"รอบและ i มีค่าเท่ากับ",(i))
+count = 0
+i = 0
+for i in range(55, 0,-11):
+    print(i)
+    count += 1
+print("ลูป 5 ทำงาน",(count),"รอบและ i มีค่าเท่ากับ",(i))
+count = 0
+i = 0
+for i in range(1, 0):
+    print(i)
+    count += 1
+print("ลูป 6 ทำงาน",(count),"รอบและ i มีค่าเท่ากับ",(i))
+count = 0
+i = 0
